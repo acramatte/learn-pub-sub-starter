a93f8cff3d1789e71a385e169b718f2d84e09243
Alexis Cramatte <alexis.cramatte@comerge.net>
2024-12-12 22:45:39 +0800

    initial dial to rabbitmq
    - install Go AMQP RabbitMQ client a setup the dial connection in our main server
    - register a channel that gets notify of an os.Interrupt signal (Ctrl+c) and block the execution of the program until the interupt signal is received

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+)
 
 func main() {
+	url := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(url)
+	if err != nil {
+		fmt.Errorf("Error Dialing %v", err)
+	}
+	defer connection.Close()
+	fmt.Println("Connection successful")
+
 	fmt.Println("Starting Peril server...")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
