99da2583b6e1e3fb52c035a4139b24a2eeb2ec4e
Alexis Cramatte <alexis.cramatte@comerge.net>
2024-12-13 12:20:19 +0800

    publish pause key to exchange
    - create a new publishing function that publishes a PauseKey routing key to the peril_direct created in the management UI
    - refactor some ErrorF to log.Fatalf in the main server to make sure the program quits if it cannot connect to RabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"fmt"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
 	"os"
 	"os/signal"
 )
 
 func main() {
-	url := "amqp://guest:guest@localhost:5672/"
-	connection, err := amqp.Dial(url)
+	fmt.Println("Starting Peril server...")
+
+	const rabbitURL = "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(rabbitURL)
 	if err != nil {
-		fmt.Errorf("Error Dialing %v", err)
+		log.Fatalf("Error Dialing to RabbitMQ %v", err)
 	}
 	defer connection.Close()
-	fmt.Println("Connection successful")
+	fmt.Println("Connection to RabbitMQ successful")
 
-	fmt.Println("Starting Peril server...")
+	channel, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("Error creating new channel %v", err)
+	}
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Printf("Error publishing to channel: %v", err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
